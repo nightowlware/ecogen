eed77ace76b8863c80b70ce2a834670c5bf1f301
shafik23 <devnull@localhost>
2016-09-02 00:41:35 -0700

    Removed sprintf dependency - still broken.

--- ecogen.js
+++ ecogen.js
@@ -1,63 +1,76 @@
 "use strict"
-var sprintf = require("sprintf-js").sprintf;
 
 
 function fatalError(msg, row, col) {
-    console.error("ERROR: ", msg);
-    console.error("ROW: ", row+1);
-    console.error("COL: ", col+1);
-    process.exit(-1);
+  console.error("ERROR: ", msg);
+  console.error("ROW: ", row+1);
+  console.error("COL: ", col+1);
+  process.exit(-1);
 }
 
 
 class Character {
-    constructor(chr, row, col) {
-        this.chr = chr;
-        this.row = row;
-        this.col = col;
-    }
+  constructor(chr, row, col) {
+    this.chr = chr;
+    this.row = row;
+    this.col = col;
+  }
 
-    toString() {
-        return sprintf("CHARACTER: char=%s row=%s col=%s",  this.chr, this.row, this.col);
-    }
+  toString() {
+    return `CHARACTER: char=${this.chr} row=${this.row} col=${this.col}`;
+  }
 }
 
 
 class Scanner {
-    constructor(text) {
-        this.text = text;
-        this.row = 0;
-        this.col = 0;
-        this.index = 0;
+  constructor(text) {
+    this.text = text;
+    this.row = 0;
+    this.col = 0;
+    this.index = 0;
+  }
+
+  next() {
+    if (this.index > text.length - 1) {
+      return null;
     }
 
-    next() {
-        if (this.index > text.length - 1) {
-            return null;
-        }
-
-        let c = new Character(this.text[this.index], this.row, this.col);
-        this.index++;  
-        if (c.chr == '\n') {
-            this.row++;
-            this.col = 0;
-        } 
-        else { this.col++; }
-
-        return c;
+    let c = new Character(this.text[this.index], this.row, this.col);
+    this.index++;
+    if (c.chr == '\n') {
+      this.row++;
+      this.col = 0;
     }
+    else { this.col++; }
+
+    return c;
+  }
 }
 
 
-class Token {
-    constructor() {
-        this.type = null;
-        this.text = "";
-        this.row = 0;
-        this.col= 0;
-    }
+TOKEN_JS_GENERIC = "JS_GENERIC"
+TOKEN_JS_LINE = "JS_LINE"
+TOKEN_JS_COLON = "JS_COLON"
+TOKEN_JS_NEWLINE = "JS_NEWLINE"
+TOKEN_JS_BLOCK_END = "JS_BLOCK_END"
+TOKEN_JS_EXPRESSION = "JS_EXPRESSION"
+TOKEN_JS_CHUNK = "CHUNK"
 
-    toString() {
-        return sprintf("TOKEN type=%s, text=%s, row=%s, col=%s", this.type, this.text, this.row, this.col);
-    }
+class Token {
+  constructor() {
+    this.type = null;
+    this.text = "";
+    this.row = 0;
+    this.col= 0;
+  }
+
+  toString() {
+    return `TOKEN type=${this.type}, text=${this.text}, row=${this.row}, col=${this.col}`;
+  }
+}
+
+
+class Lexer {
+  constructor() {
+  }
 }
