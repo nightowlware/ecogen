e08cba319f8288786e3bf211ba0c99d26aaccae2
Shafik Amin <samin@carcharging.com>
2016-09-16 19:26:44 -0700

    Major refactor - XP with Anderson!

--- ecogen.js
+++ ecogen.js
@@ -51,8 +51,12 @@ class Scanner {
     this.index = 0;
   }
 
+  hasNext() {
+    return !(this.index > this.text.length - 1);
+  }
+
   next() {
-    if (this.index > this.text.length - 1) {
+    if (!this.hasNext()) {
       return null;
     }
 
@@ -66,6 +70,11 @@ class Scanner {
 
     return c;
   }
+
+  // Note: this returns a naked string, not a Character object
+  peek() {
+    return this.hasNext() ? this.text[this.index] : null;
+  }
 }
 
 
@@ -78,7 +87,7 @@ const TOKEN_CHUNK = "CHUNK"
 
 class Token {
   constructor() {
-    this.type = null;
+    this.type = TOKEN_CHUNK;
     this.text = "";
     this.row = 0;
     this.col= 0;
@@ -86,6 +95,10 @@ class Token {
 
   toString() {
     return `TOKEN type=${this.type}, text=${evalEscape(this.text)}, row=${this.row}, col=${this.col}`;
+  }
+
+  debugData() {
+    return `// --- TJS line ${this.row + 1}`;
   }
 }
 
@@ -103,54 +116,61 @@ class Lexer {
 
     while (c) {
 
-      if (!token.type) {
-        if (c.char === '~') {
+      if (token.type === TOKEN_CHUNK) {
+        if (c.col === 0 && c.char === '~') {
+          if (token.text.length > 0) {
+            tokens.push(token);
+          }
+          token = new Token();
           token.type = TOKEN_TJS_LINE;
+          token.row = c.row;
+          token.col = c.col;
+
+          // Skip over the '~'
           c = this.scanner.next();
-          while (c.char === ' ') {
+          // Consume white space
+          while (c.char === ' ' || c.char === '\t') {
             c = this.scanner.next();
           }
-        } else {
-          token.type = TOKEN_CHUNK;
         }
-        token.row = c.row;
-        token.col = c.col;
-      }
-
-      else if (token.type === TOKEN_CHUNK) {
-        if (c.char === '~') {
+        else if (c.char === '#' && this.scanner.peek() === '|') {
           tokens.push(token);
           token = new Token();
-        } else {
+          token.type = TOKEN_TJS_EXPRESSION;
+          token.row = c.row;
+          token.col = c.col;
+
+          // Skip over the '|'
+          this.scanner.next();
+
+          c = this.scanner.next();
+        }
+        else {
+          if (token.text.length === 0) {
+            token.row = c.row;
+            token.col = c.col;
+          }
           token.text += c.char;
           c = this.scanner.next();
         }
       }
 
       else if (token.type === TOKEN_TJS_LINE) {
-        if (c.char === '#') {
-          if (token.text === '') {
-            token.type = TOKEN_TJS_EXPRESSION;
-          } else {
-            token.text += c.char;
-          }
-        }
-
-        else if (c.char === '\n') {
-            token.text += c.char;
-            tokens.push(token);
-            token = new Token();
-        }
-
-        else {
+        if (c.char === '\n') {
+          token.text += '    ' + token.debugData() + '\n';
+          tokens.push(token);
+          token = new Token();
+        } else {
           token.text += c.char;
         }
-
         c = this.scanner.next();
       }
 
       else if (token.type === TOKEN_TJS_EXPRESSION) {
-        if (c.char === '#') {
+        if (c.char === '|' && this.scanner.peek() === '#') {
+          // We need to advance the scanner by one char at this point,
+          // in order to skip the closing '#'
+          this.scanner.next();
           tokens.push(token);
           token = new Token();
         } else {
@@ -223,43 +243,52 @@ class Runner {
 }
 
 
-function parse(source, env) {
+function run(source, env) {
   return new Runner(new Generator(new Lexer(new Scanner(source))), env).run();
+}
+
+
+
+// node module exports
+module.export = {
+  Scanner,
+  Lexer,
+  Generator,
+  Runner,
+  run
 }
 
 
 
 // Test
 if (process.argv[2] === 'test') {
-  const input0 =
+  let scripts = [];
+
+  scripts.push(
 `
 ~for (let i = 0; i < 5; i++) {
-  console.log(~#i#);
-  console.log('This is also a quoted string');
-~}
-`;
+  Problem #|i*i + i|# thing
+~}`);
 
- const input1 =
+ scripts.push(
 `
 ~for (let i = 0; i < 5; i++) {
-  console.log("The value of i is:", ~#i#);
-  console.log('Another time:', ~#i#);
-~}
-`;
+  console.log("The value of i is:", #|i|#);
+  console.log('Another time:', #|i|#);
+~}`);
 
-
- const input2 =
+ scripts.push(
 `
 ~// This is a TJS comment
 ~var i = 42;
 ~          let a = 5;
 ~let name = 'Shafik';
 ~let d = [1,2,3];
-The value of d is: ~#d.toString()#
-I think that ~#new String(i)# is the meaning of life.
-Hello, my name is ~#name# and I am a person.
+The value of d is: #|d.toString()|#
+I think that #|new String("something")|# is the meaning of life.
+Hello, my name is #|name|# and I am a person.
 ~for (let i = 0; i < a; i++) {
-  loop index: ~#i#
+  loop index: #|i|#
 ~}
 ~ //thing: comment: ##
 ~
@@ -271,32 +300,36 @@ t is greater than 6
 ~}
 ~else {
 t is 4, 5, or 6
-~}
-`;
+~}`);
 
   const expr="foo(baz(bar(3)))";
-  const input3 =
+  scripts.push(
 `
 ~let k = 4;
 ~function foo(x) { return x/k; }
 ~function bar(x) { return x*k; }
 ~function baz(x) { return x+k; }
 
-Result of ${expr}: ~#${expr}#
-`;
+Result of ${expr}: #|${expr}|#`);
 
-  const s = new Scanner(input1);
-  const l = new Lexer(s);
-  const g = new Generator(l);
+  // scripts = scripts.slice(0, 1);
+  // scripts = [scripts[2]];
 
-  // Turn on to debug tokenizer
-  if (false) {
-    const tokens = l.lex();
-    for (const token of tokens) {
-      console.log("token: ", token);
-      console.log("----------------------------------");
+  for (const script of scripts) {
+    const s = new Scanner(script);
+    const l = new Lexer(s);
+    const g = new Generator(l);
+
+    // Turn on to debug tokenizer
+    if (false) {
+      const tokens = l.lex();
+      for (const token of tokens) {
+        console.log("token: ", token);
+        console.log("----------------------------------");
+      }
     }
-  }
 
-  console.log(new Runner(g, this).run());
+    console.log(new Runner(g, this).run());
+    console.log("----------------------------------");
+  }
 }
---
+++ test.js
@@ -0,0 +1,1 @@
+ecogen = require('./ecogen');
